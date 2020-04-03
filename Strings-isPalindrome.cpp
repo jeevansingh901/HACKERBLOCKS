148092ea7ba2545fbb5f445ae809f10ef1daab60
jeevansingh901 <42614183+jeevansingh901@users.noreply.github.com>
2020-04-03 23:43:06 +0530

    Create Strings-isPalindrome.cpp

---
+++ Strings-isPalindrome.cpp
@@ -0,0 +1,24 @@
+//Strings-isPalindrome
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main() {
+	string s;
+	cin>>s;
+	int flag=1;
+	int n=s.length();
+	for(int i=0;i<s.length();i++){
+		if(s[i]!=s[n-i-1]){
+         flag=0;
+		 break;
+		}
+	}
+	if(flag==0){
+		cout<<"false";
+	}
+
+  if(flag==1){
+	  cout<<"true";
+  }
+	return 0;
+}
